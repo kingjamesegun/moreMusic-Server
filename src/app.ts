b60e3299cb730cc8bc5c61de8bb13e0d7d7a9849
kingjamesegun <kingjamesegun@gmail.com>
2024-03-11 00:03:26 +0100

    Boilerplate Node

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
